f21c9b1b36f5550fececc3839e369e6af3d1ebd4
connor <mcwvproductions@gmail.com>
2019-03-07 01:14:14 -0800

    bruh sound works, toast doesn't

--- app/src/main/java/com/example/bruhtton/MainActivity.java
+++ app/src/main/java/com/example/bruhtton/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.bruhtton;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button brtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        brtn = (Button)findViewById(R.id.btnBruh);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bruh);
+        brtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mp.start();
+            }
+        });
     }
 
     public void bruh (View view) {
